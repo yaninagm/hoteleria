151fa6fb94921ffbcb044962569377c539241141
adamantike <adamantike@users.noreply.github.com>
2014-11-12 10:42:16 -0300

    Redimensión dinámica de imagen limitada a tamaño original

--- public_html/js/funciones.js
+++ public_html/js/funciones.js
@@ -118,9 +118,8 @@ $(function() {
                 var hotel    = id_hotel;
                 $.each(data, function(index, item) {
                     if (item.id == hotel) {
-                        cabecera += '<div style="width: 100%;">'
-                        //cabecera += '<img src="' + item.enlace_imagen + '" style="display: block; margin-left: auto; margin-right: auto;" />';
-                        cabecera += '<img src="' + item.enlace_imagen + '" style="width: 100%;" />';
+                        cabecera += '<div style="width: 100%; text-align: center;">'
+                        cabecera += '<img src="' + item.enlace_imagen + '" style="max-width: 100%;" />';
                         cabecera += '</div>'
                         cabecera += '<h1 style="text-align: center">' + item.nombre + '</h2>';
                         if (item.info_resumen) {
